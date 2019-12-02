41e35d29a2208597cf563dc4475a4a0cbbd4c1ab
mcipekci <48002757+mcipekci@users.noreply.github.com>
2019-12-02 06:12:06 +0300

    Minor changes

--- cc.sh
+++ cc.sh
@@ -26,15 +26,15 @@ do
         currentdatabase=$(echo $currentData | jq -c '.["name"]' | sed 's/\"//g')
         echo "Searching $domain on $currentdatabase"
         targeturl=$(echo $currentData | jq -c '.["cdx-api"]' | sed 's/\"//g')
-        fetchedData=$(curl -s "$targeturl?url=*.$domain/*&$outputStyle" | jq -c .url)
-        if [[ $fetchedData != "null" ]]; then
-                results+="$fetchedData "
+        fetchedData=$(curl -s "$targeturl?url=*.$domain/*&$outputStyle" | jq -c '. | select(.url != null) | .url')
+        if [[ ! -z "$fetchedData" ]]; then
+                results+="$fetchedData"
         fi
 done
 echo "-----------------------------------------------------"
 echo "Fetched all databases for $domain"
 filename="CC-$domain-$(date "+%Y.%m.%d-%H.%M").txt"
-echo $results | jq . | sed 's/\"//g' | sort -u > $filename
+echo "$results" | jq . | sed 's/\"//g' | sort -u > $filename
 count=$(wc -l $filename | awk {'print $1'})
 echo "Saved output on: $filename"
 echo "Total urls found: $count"
